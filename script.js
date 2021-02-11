6421661ab7ddf08d42c5956d1b1ba2389af25a34
Keon Pourboghrat <kekons4@gmail.com>
2021-02-10 22:34:01 -0500

    updated the logic table which determines what the user preferences

--- script.js
+++ script.js
@@ -16,7 +16,10 @@ function generatePassword() {
   let output = "";
   
   // used to generate the random password 
-  let charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=\|}]{[:;?/><";
+  let lowerSet = "abcdefghijklmnopqrstuvwxyz";
+  let upperSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let numSet = "0123456789";
+  let specialSet = "!@#$%^&*()_-+=\|}]{[:;?/><";
 
   // prompts user to enter the desired length for password
   let passwordLen = Number(prompt("Enter a length between 8 and 128 characters: "));
@@ -46,10 +49,125 @@ function generatePassword() {
   
   // depending on the length of password the for loop will iterate.
   for(let i = 0; i < passwordLen; i++) {
+
+    // Logic table that goes through all possible outcomes of what the user can choose
     
-    // add a random character to output 
-    output += charSet.charAt(Math.floor(Math.random() * charSet.length));    
-  
+    // Lowercase: false | uppercase: false | numbers: false | special characters: true
+    if(lower === false && upper === false && numeric === false && special === true) {
+      output += specialSet.charAt(Math.floor(Math.random() * specialSet.length));
+    } 
+    // Lowercase: false | uppercase: false | numbers: true | special characters: false
+    else if(lower === false && upper === false && numeric === true && special === false) {
+      output += numSet.charAt(Math.floor(Math.random() * numSet.length));
+    }
+    // Lowercase: false | uppercase: false | numbers: true | special characters: true
+    else if(lower === false && upper === false && numeric === true && special === true) {
+      let random = Math.floor(Math.random() * 2);
+      if(random === 0) {
+        output += specialSet.charAt(Math.floor(Math.random() * specialSet.length));
+      } else {
+        output += numSet.charAt(Math.floor(Math.random() * numSet.length));
+      } 
+    } 
+    // Lowercase: false | uppercase: true | numbers: false | special characters: false
+    else if(lower === false && upper === true && numeric === false && special === false) {
+      output += upperSet.charAt(Math.floor(Math.random() * upperSet.length));
+    } 
+    // Lowercase: false | uppercase: true | numbers: false | special characters: true
+    else if(lower === false && upper === true && numeric === false && special === true) {
+      let random = Math.floor(Math.random() * 2);
+      if(random === 0) {
+        output += specialSet.charAt(Math.floor(Math.random() * specialSet.length));
+      } else {
+        output += upperSet.charAt(Math.floor(Math.random() * upperSet.length));
+      } 
+    } 
+    // Lowercase: false | uppercase: true | numbers: true | special characters: false
+    else if(lower === false && upper === true && numeric === true && special === false) {
+      let random = Math.floor(Math.random() * 2);
+      if(random === 0) {
+        output += numSet.charAt(Math.floor(Math.random() * numSet.length));
+      } else {
+        output += upperSet.charAt(Math.floor(Math.random() * upperSet.length));
+      } 
+    } 
+    // Lowercase: false | uppercase: true | numbers: true | special characters: true
+    else if(lower === false && upper === true && numeric === true && special === true) {
+      let random = Math.floor(Math.random() * 3);
+      if(random === 0) {
+        output += specialSet.charAt(Math.floor(Math.random() * specialSet.length));
+      } else if(random === 1) {
+        output += numSet.charAt(Math.floor(Math.random() * numSet.length));
+      } else {
+        output += upperSet.charAt(Math.floor(Math.random() * upperSet.length));
+      }
+    } 
+    // Lowercase: true | uppercase: true | numbers: true | special characters: false
+    else if(lower === true && upper === false && numeric === false && special === false) {
+      output += lowerSet.charAt(Math.floor(Math.random() * lowerSet.length));
+    } 
+    // Lowercase: true | uppercase: false | numbers: false | special characters: true
+    else if(lower === true && upper === false && numeric === false && special === true) {
+      let random = Math.floor(Math.random() * 2);
+      if(random === 0) {
+        output += specialSet.charAt(Math.floor(Math.random() * specialSet.length));
+      } else {
+        output += lowerSet.charAt(Math.floor(Math.random() * lowerSet.length));
+      } 
+    } 
+    // Lowercase: true | uppercase: false | numbers: true | special characters: false
+    else if(lower === true && upper === false && numeric === true && special === false) {
+      let random = Math.floor(Math.random() * 2);
+      if(random === 0) {
+        output += numSet.charAt(Math.floor(Math.random() * numSet.length));
+      } else {
+        output += lowerSet.charAt(Math.floor(Math.random() * lowerSet.length));
+      } 
+    } 
+    // Lowercase: true | uppercase: false | numbers: true | special characters: true
+    else if(lower === true && upper === false && numeric === true && special === true) {
+      let random = Math.floor(Math.random() * 3);
+      if(random === 0) {
+        output += specialSet.charAt(Math.floor(Math.random() * specialSet.length));
+      } else if(random === 1) {
+        output += numSet.charAt(Math.floor(Math.random() * numSet.length));
+      } else {
+        output += lowerSet.charAt(Math.floor(Math.random() * lowerSet.length));
+      }
+    } 
+    // Lowercase: true | uppercase: true | numbers: true | special characters: true
+    else if(lower === true && upper === true && numeric === false && special === false) {
+      let random = Math.floor(Math.random() * 2);
+      if(random === 0) {
+        output += upperSet.charAt(Math.floor(Math.random() * upperSet.length));
+      } else {
+        output += lowerSet.charAt(Math.floor(Math.random() * lowerSet.length));
+      } 
+    } 
+    // Lowercase: true | uppercase: true | numbers: false | special characters: true
+    else if(lower === true && upper === true && numeric === false && special === true) {
+      let random = Math.floor(Math.random() * 3);
+      if(random === 0) {
+        output += specialSet.charAt(Math.floor(Math.random() * specialSet.length));
+      } else if(random === 1) {
+        output += upperSet.charAt(Math.floor(Math.random() * upperSet.length));
+      } else {
+        output += lowerSet.charAt(Math.floor(Math.random() * lowerSet.length));
+      }
+    } 
+    // Lowercase: true | uppercase: true | numbers: true | special characters: true
+    else if(lower === true && upper === true && numeric === true && special === true) {
+      let random = Math.floor(Math.random() * 4);
+      if(random === 0) {
+        output += specialSet.charAt(Math.floor(Math.random() * specialSet.length));
+      } else if(random === 1) {
+        output += numSet.charAt(Math.floor(Math.random() * numSet.length));
+      } else if(random === 2) {
+        output += upperSet.charAt(Math.floor(Math.random() * upperSet.length));
+      } else {
+        output += lowerSet.charAt(Math.floor(Math.random() * lowerSet.length));
+      }
+    }   
   }
 
   // return ouput
